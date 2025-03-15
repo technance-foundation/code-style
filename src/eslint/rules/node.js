d9072cc32d5443ccb866d6b21c8292e373d4a35b
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2025-03-15 13:55:26 +0330

    Implement `node` specific rules

---
+++ src/eslint/rules/node.js
@@ -0,0 +1,7 @@
+//@ts-check
+
+/** @type {import('eslint').Linter.RulesRecord} */
+export const nodeRules = {
+    /** Enforce usage of the `node:` prefix for builtin imports */
+    "node/prefer-node-protocol": "error",
+};
