3b7879384b3bfb7674ac6ee9b170ca3b6cc41cb0
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2025-03-17 00:47:25 +0330

    Implement ESLint config for Nest.js projects

--- src/eslint/rules/index.js
+++ src/eslint/rules/index.js
@@ -4,3 +4,4 @@ export { javascriptRules } from "./javascript.js";
 export { reactRules } from "./react.js";
 export { nodeRules } from "./node.js";
 export { typescriptRules } from "./typescript.js";
+export { nestRules } from "./nest.js";
